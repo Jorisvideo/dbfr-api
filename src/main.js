a0dd0f8360a3b7757c707254a50250a9ea78697a
TheShooter <30504258+TheShooter71@users.noreply.github.com>
2018-03-31 17:01:47 +0200

    add get

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const snekfetch = require('snekfetch');
+const request = require('request');
 
 class dbfr {
 
@@ -17,12 +18,30 @@ class dbfr {
       });
     }
 
- post(client, key) {
+ async post(client, key) {
     snekfetch.post(`https://discordbots.fr/api/v1/bot/${this.client.user.id}`, { headers: { Authorization: this.key } })
       .send({ server_count: this.client.guilds.size,
 server_shard: this.client.options.shardCount})
       .catch(err => console.log('[Discordbots.fr] Erreur:' + err.text));
   }
+
+ async get(args) {
+  return new Promise((resolve, reject) => {
+    request(`https://discordbots.fr/api/v1/bot/${args}`, (err, res, body) => {
+
+                    if(res.statusCode === 404) return console.log('[Discordbots.fr] Request Failed');
+
+                    if(err || res.statusCode!== 200) return
+
+                      console.log('chargé avec succés');
+
+                        let objet = JSON.parse(body);
+
+                      resolve(objet)
+
+     })
+  })
+}
 }
 
 module.exports = dbfr;
