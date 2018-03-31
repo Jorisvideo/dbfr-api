0acb8a11da237c383c27d05a3537d19c8f8622d0
Mathias B <l4p1n@l4p1n.met-hardware.fr>
2018-03-31 19:36:53 +0200

    Cleanup and replaced lib

--- src/main.js
+++ src/main.js
@@ -18,25 +18,36 @@ class dbfr {
   }
 
   async post(client, key) {
-    snekfetch.post(`https://discordbots.fr/api/v1/bot/${this.client.user.id}`, { headers: { Authorization: this.key } })
-      .send({
-        server_count: this.client.guilds.size,
-        server_shard: this.client.options.shardCount
-      })
-      
-      .catch(err => console.log('[Discordbots.fr] Erreur:' + err.text));
+    try{
+      return await request({ // Will return an object
+        uri: `https://https://discordbots.fr/api/v1/bot/${this.client.user.id}`,
+        method: 'POST',
+        headers: {
+          "Authorization": this.key,
+        },
+  
+        body: {
+          server_count: this.client.guilds.size,
+          server_shard: this.client.options.shardCount
+        },
+  
+        json: true
+      });
+    }catch(err){
+      throw err; // Throw directly and let the user handle the error
+    }
   }
 
   async get(args) {
-    return new Promise((resolve, reject) => {
-      request(`https://discordbots.fr/api/v1/bot/${args}`, (err, res, body) => {
-        if(res.statusCode === 404) return console.log('[Discordbots.fr] Request Failed');
-        if(err || res.statusCode!== 200) return
-        console.log('chargé avec succés');
-        let objet = JSON.parse(body);
-        resolve(objet)
+    try{
+      return await request({
+        uri: `https://discordbots.fr/api/v1/bot/${args}`,
+        
+        json: true
       })
-    })
+    }catch(err){
+      throw err;
+    }
   }
 }
 
