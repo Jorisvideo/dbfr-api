618d9b83e2cc04503cf95e52a10b71767c226559
Mathias B <l4p1n@l4p1n.met-hardware.fr>
2018-03-31 18:58:56 +0200

    Fix ugly indentation

--- src/main.js
+++ src/main.js
@@ -11,37 +11,34 @@ class dbfr {
     this.key = key;
 
     this.client.on('ready', () => {
-    this.post()
-        setInterval(() => {
-          this.post();
-        }, 60000);
-      });
-    }
+      this.post()
+      setInterval(() => {
+        this.post();
+      }, 60000);
+    });
+  }
 
- async post(client, key) {
+  async post(client, key) {
     snekfetch.post(`https://discordbots.fr/api/v1/bot/${this.client.user.id}`, { headers: { Authorization: this.key } })
-      .send({ server_count: this.client.guilds.size,
-server_shard: this.client.options.shardCount})
+      .send({
+        server_count: this.client.guilds.size,
+        server_shard: this.client.options.shardCount
+      })
+      
       .catch(err => console.log('[Discordbots.fr] Erreur:' + err.text));
   }
 
- async get(args) {
-  return new Promise((resolve, reject) => {
-    request(`https://discordbots.fr/api/v1/bot/${args}`, (err, res, body) => {
-
-                    if(res.statusCode === 404) return console.log('[Discordbots.fr] Request Failed');
-
-                    if(err || res.statusCode!== 200) return
-
-                      console.log('chargé avec succés');
-
-                        let objet = JSON.parse(body);
-
-                      resolve(objet)
-
-     })
-  })
-}
+  async get(args) {
+    return new Promise((resolve, reject) => {
+      request(`https://discordbots.fr/api/v1/bot/${args}`, (err, res, body) => {
+        if(res.statusCode === 404) return console.log('[Discordbots.fr] Request Failed');
+        if(err || res.statusCode!== 200) return
+        console.log('chargé avec succés');
+        let objet = JSON.parse(body);
+        resolve(objet)
+      })
+    })
+  }
 }
 
 module.exports = dbfr;
