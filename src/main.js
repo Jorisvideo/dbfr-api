f0372be2c46899f881f7c8ef4da35f981895c07b
TheShooter <30504258+TheShooter71@users.noreply.github.com>
2018-04-01 12:57:42 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-const snekfetch = require('snekfetch');
 const request = require('request');
 
 class dbfr {
 
   constructor(client, key) {
     
-    if(!client) return console.error(`[SYSTEM INFO] Cette API n\'est pas reconnu par le module`);
+    if(!client) return console.error(`[dbfr-api] Cette API n\'est pas reconnu par le module`);
 
     this.client = client;
     this.key = key;
@@ -19,17 +18,25 @@ class dbfr {
     }
 
  async post(client, key) {
-    snekfetch.post(`https://discordbots.fr/api/v1/bot/${this.client.user.id}`, { headers: { Authorization: this.key } })
-      .send({ server_count: this.client.guilds.size,
-server_shard: this.client.options.shardCount})
-      .catch(err => console.log('[Discordbots.fr] Erreur:' + err.text));
+     request.post({
+        headers: {
+            "Authorization": this.key,
+        },
+        url: `https://discordbots.fr/api/v1/bot/${this.client.user.id}`,
+        json: true,
+        body: {"server_count": ,
+"server_shard": this.client.options.shardCount}
+    }, function (err, resp, body) {
+        console.log(body);
+});
   }
+
 
  async get(args) {
   return new Promise((resolve, reject) => {
     request(`https://discordbots.fr/api/v1/bot/${args}`, (err, res, body) => {
 
-                    if(res.statusCode === 404) return console.log('[Discordbots.fr] Request Failed');
+                    if(res.statusCode === 404) return console.log('[dbfr-api] Request Failed');
 
                     if(err || res.statusCode!== 200) return
 
